16e3d1029412f3dd1ee9ebccf2b43d533f086075
kayra <157609274+ibrahimKayraDural@users.noreply.github.com>
2024-07-31 16:47:55 +0300

    TileScript

---
+++ Assets/Scripts/Oveworld/TileScript.cs
@@ -0,0 +1,11 @@
+namespace Overworld
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class TileScript : MonoBehaviour
+    {
+        
+    }
+}
