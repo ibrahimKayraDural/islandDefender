721acd458ad96b19066371e462238f9f1c973bf6
kayra <157609274+ibrahimKayraDural@users.noreply.github.com>
2024-12-18 14:28:28 +0300

    Manual Turret on Rails added

--- Assets/Scripts/TowerDefence/Turrets/Turrets/Turret_BasicManual.cs
+++ Assets/Scripts/TowerDefence/Turrets/Turrets/Turret_BasicManual.cs
@@ -39,22 +39,22 @@ namespace TowerDefence
 
         internal override void OnUpdate()
         {
-            if (_lookTransform != null)
-            {
-                Quaternion rot = Quaternion.LookRotation(_lookTransform.position - _RotatingHead.position, Vector3.up);
-                float targetRot = rot.eulerAngles.y;
-                Vector3 targetVector = Vector3.zero;
+            //    if (_lookTransform != null)
+            //    {
+            //        Quaternion rot = Quaternion.LookRotation(_lookTransform.position - _RotatingHead.position, Vector3.up);
+            //        float targetRot = rot.eulerAngles.y;
+            //        Vector3 targetVector = Vector3.zero;
 
-                switch (_TurnAxis)
-                {
-                    case TurnAxis.X: targetVector.x = targetRot; break;
-                    case TurnAxis.Y: targetVector.y = targetRot; break;
-                    case TurnAxis.Z: targetVector.z = targetRot; break;
-                }
+            //        switch (_TurnAxis)
+            //        {
+            //            case TurnAxis.X: targetVector.x = targetRot; break;
+            //            case TurnAxis.Y: targetVector.y = targetRot; break;
+            //            case TurnAxis.Z: targetVector.z = targetRot; break;
+            //        }
 
 
-                _RotatingHead.localEulerAngles = targetVector;
-            }
+            //        _RotatingHead.localEulerAngles = targetVector;
+            //    }
         }
 
         internal override void ActivationMethod()
@@ -62,20 +62,23 @@ namespace TowerDefence
             if (_projectileIsValid == false) return;
             if (_isInitialized == false) return;
 
-            Vector3 targetPoint = _lookTransform.position;
-            Ray ray = new Ray(_Barrel.position, -_plane.normal);
-            if (_plane.Raycast(ray, out float enter))
-            {
-                Vector3 hitPoint = ray.GetPoint(enter);
+            GameObject go = Instantiate(_ProjectilePrefab, _Barrel.position, Quaternion.identity);
+            go.GetComponent<Projectile>().Initialize(Vector3.forward, _data);
 
-                hitPoint += _plane.normal * _ProjectileElevation;
-                targetPoint += _plane.normal * _ProjectileElevation;
+            //Vector3 targetPoint = _lookTransform;
+            //Ray ray = new Ray(_Barrel.position, -_plane.normal);
+            //if (_plane.Raycast(ray, out float enter))
+            //{
+            //    Vector3 hitPoint = ray.GetPoint(enter);
 
-                GameObject go = Instantiate(_ProjectilePrefab, hitPoint, Quaternion.identity);
-                go.GetComponent<Projectile>().Initialize(targetPoint - hitPoint, _data);
+            //    hitPoint += _plane.normal * _ProjectileElevation;
+            //    targetPoint += _plane.normal * _ProjectileElevation;
 
-                _audioManager.PlayClip(_shootClipID, _ShootClip);
-            }
+            //    GameObject go = Instantiate(_ProjectilePrefab, hitPoint, Quaternion.identity);
+            //    go.GetComponent<Projectile>().Initialize(targetPoint - hitPoint, _data);
+
+            //    _audioManager.PlayClip(_shootClipID, _ShootClip);
+            //}
         }
 
         internal override void OnInitialized()
