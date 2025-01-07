1f952ff8774348019eaa7c8279f031ed661efe07
EfeBektas <126690737+EfeBektas@users.noreply.github.com>
2025-01-07 19:50:22 +0300

    Collissions and minable tree

---
+++ Assets/Scripts/Managers/MapIconManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapIconManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject Icon;
+
+    public void SetStraight()
+    {
+        
+    }
+}
