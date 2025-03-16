9724539c02860d9d38ad4d38e892d32bdc49c74f
kayra <157609274+ibrahimKayraDural@users.noreply.github.com>
2025-03-16 16:01:51 +0300

    Upgrade system tweaks

--- Assets/_Scripts/UI/PlayerUpgradeManager.cs
+++ Assets/_Scripts/UI/PlayerUpgradeManager.cs
@@ -10,18 +10,23 @@ using UnityEngine.UI;
 public class PlayerUpgradeManager : MonoBehaviour
 {
     [System.Serializable]
+    struct UpgradeTree
+    {
+        public List<UpgradeUIPiece> Upgrades;
+        public Slider Slider;
+        public string ID;
+    }
+    [System.Serializable]
     struct UpgradeUIPiece
     {
         public Button @Button;
         public UpgradeData Upgrade;
         public List<Cost> BuyCost;
         public float SliderAfterValue;
-        public int Level;
-        public string ID;
+        [HideInInspector] public UpgradeTree Owner;
     }
 
-    [SerializeField] List<UpgradeUIPiece> _Upgrades;
-    [SerializeField] List<SerializedKeyValuePair<string, Slider>> _SlidersWithID;
+    [SerializeField] List<UpgradeTree> _Upgrades;
     [SerializeField] TextMeshProUGUI _Title;
     [SerializeField] TextMeshProUGUI _Description;
     [SerializeField] TextMeshProUGUI _Cost;
@@ -40,58 +45,47 @@ public class PlayerUpgradeManager : MonoBehaviour
     BaseResourceController AUTO_BaseResourceController = null;
 
     UpgradeUIPiece? _selectedUpgrade;
-    Dictionary<string, List<UpgradeUIPiece>> _dataDictionary;
 
     void Awake()
     {
-        foreach (var item in _Upgrades)
+        foreach (var tree in _Upgrades)
         {
-            item.Button.onClick.AddListener(delegate { OnUpgradeSelected(item); });
-        }
-
-        _dataDictionary = new Dictionary<string, List<UpgradeUIPiece>>();
-        foreach (var item in _Upgrades)
-        {
-            if (_dataDictionary.ContainsKey(item.ID)) _dataDictionary[item.ID].Add(item);
-            else _dataDictionary.Add(item.ID, new List<UpgradeUIPiece>() { item });
-        }
-
-        foreach (var dataList in _dataDictionary.Values)
-        {
-            dataList.ForEach(x => x.Button.interactable = false);
-            dataList.OrderBy(x => x.Level).First().Button.interactable = true;
+            for (int i = 0; i < tree.Upgrades.Count; i++)
+            {
+                var item = tree.Upgrades[i];
+                var i2 = i;
+                var tree2 = tree;
+                item.Button.onClick.AddListener(delegate { OnUpgradeSelected(tree, i2); });
+                item.Button.interactable = i == 0;
+                item.Owner = tree;
+            }
         }
     }
 
     public void BuyUpgrade()
     {
         if (_selectedUpgrade == null) return;
-        if (_BaseResourceController.TrySpendResource(_selectedUpgrade.Value.BuyCost.ToArray()) == false)
+        var su = _selectedUpgrade.Value;
+        if (_BaseResourceController.TrySpendResource(su.BuyCost.ToArray()) == false)
         {
             DeselectUpgrade();
             _Title.text = "<color=red>INSUFFICENT FUNDS<color=white>";
             return;
         }
 
-        HandleUpgradeEffect(_selectedUpgrade.Value);
+        HandleUpgradeEffect(su);
 
-        _selectedUpgrade.Value.Button.interactable = false;
-        _selectedUpgrade.Value.Button.GetComponent<Image>().color = _BoughtUpgradeColor;
-        var list = _dataDictionary[_selectedUpgrade.Value.ID];
-        list.Remove(_selectedUpgrade.Value);
+        su.Button.interactable = false;
+        su.Button.GetComponent<Image>().color = _BoughtUpgradeColor;
 
-        if (list.Count <= 0) _dataDictionary.Remove(_selectedUpgrade.Value.ID);
-        else list.OrderBy(x => x.Level).First().Button.interactable = true;
-
-        var slider = _SlidersWithID.Find(x => x.Key == _selectedUpgrade.Value.ID).Value;
-        slider.value = _selectedUpgrade.Value.SliderAfterValue;
+        su.Owner.Slider.value = su.SliderAfterValue;
 
         DeselectUpgrade();
     }
 
     void HandleUpgradeEffect(UpgradeUIPiece upgrade)
     {
-        string id = upgrade.ID;
+        string id = upgrade.Owner.ID;
 
         if (id == "player" || id == "drillSpeed")
         {
@@ -105,19 +99,20 @@ public class PlayerUpgradeManager : MonoBehaviour
             isu?.SetSpeedUpgrade(upgrade.Upgrade);
         }
     }
-    void OnUpgradeSelected(UpgradeUIPiece UIPiece)
+    void OnUpgradeSelected(UpgradeTree tree, int index)
     {
-        _selectedUpgrade = UIPiece;
+        UpgradeUIPiece uiPiece = tree.Upgrades[index];
+        _selectedUpgrade = uiPiece;
         _Title.text = _selectedUpgrade.Value.Upgrade.DisplayName;
         _Description.text = _selectedUpgrade.Value.Upgrade.Description;
         _Cost.text = "";
-        for (int i = 0; i < UIPiece.BuyCost.Count; i++)
+        for (int i = 0; i < uiPiece.BuyCost.Count; i++)
         {
-            Cost buyCost = UIPiece.BuyCost[i];
+            Cost buyCost = uiPiece.BuyCost[i];
             _Cost.text += buyCost.Resource.DisplayName + " X " + buyCost.Amount;
-            if (i < UIPiece.BuyCost.Count - 1) _Cost.text += "\n";
+            if (i < uiPiece.BuyCost.Count - 1) _Cost.text += "\n";
         }
-        UIPiece.Button.Select();
+        uiPiece.Button.Select();
         BuyButton.SetActive(true);
     }
     void DeselectUpgrade()
